4dc430877f87b4256a242bc3c67964d4956f2c6b
soasme <soasme@gmail.com>
2018-03-12 21:10:37 +1300

    Bump version: 0.0.1 → 0.1.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name='zetanote',
-    version='0.0.1',
+    version='0.1.0',
     url='https://github.com/soasme/zetanote',
     license='GPLv3',
     description='Zete note service.',
