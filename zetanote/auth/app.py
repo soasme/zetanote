a2a22a383c72ece38075efbddc65181f4f97d349
soasme <soasme@gmail.com>
2018-03-17 22:56:56 +1300

    implement login&logout&current_user.

--- zetanote/auth/app.py
+++ zetanote/auth/app.py
@@ -1,4 +1,42 @@
 from uuid import uuid4
 
+from flask import g, session
+from werkzeug.local import LocalProxy
+
+from .models import User
+
+
+SESSION_ID = 'sid'
+
+def login(user, permanent=True):
+    session[SESSION_ID] = user.id
+    session.permanent = permanent
+    g.current_user = user
+
+
+def logout():
+    if SESSION_ID in session:
+        del session[SESSION_ID]
+
 def generate_token():
     return uuid4().hex + uuid4().hex
+
+def get_current_user():
+    user = getattr(g, 'current_user', None)
+    if user:
+        return user
+
+    sid = session.get(SESSION_ID)
+    if not sid:
+        return None
+
+    user = User.query.get(sid)
+    if not user:
+        logout()
+        return None
+
+    g.current_user = user
+    return user
+
+
+current_user = LocalProxy(get_current_user)
